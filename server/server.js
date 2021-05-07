ec1c5828c197b5d0e672ae3a65377d9792b4cff4
Javier <javiergach@unisabana.edu.co>
2021-05-07 15:48:18 -0500

    cambios

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var path = require('path');
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+
+var con = mysql.connect({
+    host: "localhost",
+    user: "root",
+    password: "SolyLuna0802",
+    database: "laboratorio"
+});
 
 var app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 5600);
 
